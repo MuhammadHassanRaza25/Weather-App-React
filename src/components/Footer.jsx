039fa1d4ca1957a6fe94edbeb1f5b189ee24c206
Muhammad Hassan Raza <153998754+MuhammadHassanRaza25@users.noreply.github.com>
2025-11-01 05:42:21 +0500

    Weather App

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,11 +4,12 @@ import { MdEmail } from "react-icons/md";
 
 export default function Footer(){
 
+    const currentYear = new Date().getFullYear()
     
     return(
         <>
           <div className="lg:mx-7 mx-4 lg:px-5 md:px-5 px-2 lg:py-5 md:py-5 py-3 mt-14 flex lg:flex-row md:flex-row flex-col lg:gap-0 gap-4 items-center justify-between bg-white rounded-lg">
-             <p className="font-semibold lg:text-sm md:text-sm text-xs text-gray-700 text-center">© 2025 Muhammad Hasssan Raza. All rights reserved.</p>
+             <p className="font-semibold lg:text-sm md:text-sm text-xs text-gray-700 text-center">© {currentYear} Muhammad Hasssan Raza. All rights reserved.</p>
              <div className="flex gap-4 justify-center">
                 <a href={'https://github.com/MuhammadHassanRaza25'} target="_blank"><p className="text-2xl text-gray-700 rounded-full"><FaGithub/></p></a>
                 <a href={'https://www.linkedin.com/in/muhammad-hassan-raza-aab5402b7/'} target="_blank"><p className="text-2xl text-gray-700 hover:text-blue-500 rounded-full"><FaLinkedinIn/></p></a>
--- src/components/WeatherBanner.jsx
+++ src/components/WeatherBanner.jsx
@@ -4,11 +4,11 @@ export default function WeatherBanner({ time, day, data, description }) {
     return (
       <div className="bgImage shadow flex lg:flex-row md:flex-row flex-col lg:justify-between md:justify-between justify-center lg:items-end md:items-end items-center h-[400px] mt-5 lg:mx-8 mx-5">
         <div className="lg:ml-10 md:ml-10 ml-0 mb-10">
-            <div class="loader"></div>
+            <div className="loader"></div>
         </div>
 
         <div className="lg:mr-10 md:mr-10 mr-0 lg:mb-10 md:mb-10 mb-0">
-           <div class="loader"></div>
+           <div className="loader"></div>
         </div>
       </div>
     );
